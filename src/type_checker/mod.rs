a6189efb9d2801ff5d5309ba0af9e2b6f17565b5
SaumitraLohokare <saumitra.lohokare@gmail.com>
2026-07-20 18:42:02 +1000

    Moved Type Checker to separate module

--- src/type_checker/mod.rs
+++ src/type_checker/mod.rs
@@ -1,3 +1,6 @@
+mod replace_set;
+
+use replace_set::ReplaceSet;
 use std::collections::HashMap;
 
 use crate::{
@@ -9,104 +12,6 @@ use crate::{
 
 type TypeLoc = (Type, Loc);
 
-#[derive(Debug)]
-struct ReplaceSet {
-    ty_to_id: HashMap<TypeLoc, usize>,
-    id_to_ty: Vec<TypeLoc>,
-
-    parents: Vec<usize>,
-}
-
-impl ReplaceSet {
-    fn new() -> Self {
-        Self {
-            ty_to_id: HashMap::new(),
-            id_to_ty: Vec::new(),
-
-            parents: Vec::new(),
-        }
-    }
-
-    // We should add all known types first
-    fn add(&mut self, ty: Type, loc: Loc) -> usize {
-        let type_loc = (ty, loc);
-        if let Some(&id) = self.ty_to_id.get(&type_loc) {
-            return id;
-        }
-        let id = self.id_to_ty.len();
-        self.id_to_ty.push(type_loc.clone());
-        self.ty_to_id.insert(type_loc, id);
-        self.parents.push(id);
-        id
-    }
-
-    fn find(&mut self, type_id: usize) -> usize {
-        // All types should already be added
-        debug_assert!(self.id_to_ty.len() > type_id);
-
-        let mut root = type_id;
-        while self.parents[root] != root {
-            root = self.parents[root];
-        }
-
-        // Path Compression
-        let mut cur = type_id;
-        while cur != root {
-            let next = self.parents[cur];
-            self.parents[cur] = root;
-            cur = next;
-        }
-
-        root
-    }
-
-    fn union(&mut self, a: usize, b: usize) -> FloResult<()> {
-        let ra = self.find(a);
-        let rb = self.find(b);
-
-        let (ra_type, ra_loc) = &self.id_to_ty[ra];
-        let (rb_type, rb_loc) = &self.id_to_ty[rb];
-
-        match (ra_type.is_known(), rb_type.is_known()) {
-            (true, true) => {
-                if ra_type != rb_type {
-                    Err(FloErr::TypeMismatch {
-                        t1: ra_type.clone(),
-                        loc1: *ra_loc,
-                        t2: rb_type.clone(),
-                        loc2: *rb_loc,
-                    })
-                } else {
-                    Ok(())
-                }
-            }
-            (false, _) => {
-                self.parents[ra] = rb;
-                Ok(())
-            }
-            (_, false) => {
-                self.parents[rb] = ra;
-                Ok(())
-            }
-        }
-    }
-
-    fn resolve(&mut self, ty: Type, loc: Loc) -> FloResult<Type> {
-        let type_loc = (ty, loc);
-
-        let Some(&id) = self.ty_to_id.get(&type_loc) else {
-            unreachable!()
-        };
-        let root = self.find(id);
-        let (root_type, _) = self.id_to_ty[root].clone();
-
-        if root_type.is_known() {
-            Ok(root_type.clone())
-        } else {
-            Err(FloErr::UnresolvedType { loc })
-        }
-    }
-}
 
 #[derive(Debug, Clone, Copy)]
 enum Constraint {
